c5bc7e7ee9b49ee6b132d0d9ed76878b6927d1bb
Clayton Condie <clayton.condie@gmail.com>
2016-10-28 13:21:39 -0600

    reformat move logic into Node

--- ReversiAI_Python/AIGuy.py
+++ ReversiAI_Python/AIGuy.py
@@ -42,12 +42,12 @@ def readMessage(sock):
     turn = int(mensaje[0])
     print "Turn: " + str(turn)
 
-    if (turn == -999):
+    if turn == -999:
         time.sleep(1)
         sys.exit()
 
-    round = int(mensaje[1])
-    print "Round: " + str(round)
+    roundNum = int(mensaje[1])
+    print "Round: " + str(roundNum)
     t1 = float(mensaje[2])  # update of the amount of time available to player 1
     # print t1
     t2 = float(mensaje[3])  # update of the amount of time available to player 2
@@ -57,80 +57,10 @@ def readMessage(sock):
     for i in range(8):
         for j in range(8):
             state[i][j] = int(mensaje[count])
-            count = count + 1
+            count += 1
         print state[i]
 
-    return turn, round
-
-
-def checkDirection(row, col, incx, incy, me):
-    sequence = []
-    for i in range(1, 8):
-        r = row + incy * i
-        c = col + incx * i
-
-        if ((r < 0) or (r > 7) or (c < 0) or (c > 7)):
-            break
-
-        sequence.append(state[r][c])
-
-    count = 0
-    for i in range(len(sequence)):
-        if (me == 1):
-            if (sequence[i] == 2):
-                count = count + 1
-            else:
-                if ((sequence[i] == 1) and (count > 0)):
-                    return True
-                break
-        else:
-            if (sequence[i] == 1):
-                count = count + 1
-            else:
-                if ((sequence[i] == 2) and (count > 0)):
-                    return True
-                break
-
-    return False
-
-
-def couldBe(row, col, me):
-    for incx in range(-1, 2):
-        for incy in range(-1, 2):
-            if ((incx == 0) and (incy == 0)):
-                continue
-
-            if (checkDirection(row, col, incx, incy, me)):
-                return True
-
-    return False
-
-
-# generates the set of valid moves for the player; returns a list of valid moves (validMoves)
-def getValidMoves(round, me):
-    validMoves = []
-    print "Round: " + str(round)
-
-    for i in range(8):
-        print state[i]
-
-    if (round < 4):
-        if (state[3][3] == 0):
-            validMoves.append([3, 3])
-        if (state[3][4] == 0):
-            validMoves.append([3, 4])
-        if (state[4][3] == 0):
-            validMoves.append([4, 3])
-        if (state[4][4] == 0):
-            validMoves.append([4, 4])
-    else:
-        for i in range(8):
-            for j in range(8):
-                if (state[i][j] == 0):
-                    if (couldBe(i, j, me)):
-                        validMoves.append([i, j])
-
-    return validMoves
+    return turn, roundNum
 
 
 # main function that (1) establishes a connection with the server, and then plays whenever it is this player's turn
@@ -139,20 +69,20 @@ def playGame(me, thehost):
 
     sock = initClient(me, thehost)
 
-    while (True):
+    while True:
         print "Read"
         status = readMessage(sock)
 
-        if (status[0] == me):
-            print "Move";
+        if status[0] == me:
+            print "Move"
             validMoves = getValidMoves(status[1], me)
             print validMoves
 
             myMove = move(validMoves)
 
-            sel = str(validMoves[myMove][0]) + "\n" + str(validMoves[myMove][1]) + "\n";
+            sel = str(validMoves[myMove][0]) + "\n" + str(validMoves[myMove][1]) + "\n"
             print "<" + sel + ">"
-            sock.send(sel);
+            sock.send(sel)
             print "sent the message"
         else:
             print "It isn't my turn"
---
+++ ReversiAI_Python/Node.py
@@ -0,0 +1,89 @@
+class Node(object):
+    def __init__(self, state, isMax, parent):
+        self.state = state
+        self.isMax = isMax
+        self.parent = parent
+        self.bestVal = 0  # assign to value of the state
+        self.bestMove = [None, None]
+
+    def calc_value(self):
+        # get possible moves
+
+        # for each possible move
+        # at least once - get value of move
+        #
+        # if value is 'WORSE?' than our parents best ... break
+        # update personal best
+
+        pass
+
+    """
+    FROM AIguy
+    """
+
+    def checkDirection(self, row, col, incx, incy, me):
+        sequence = []
+        for i in range(1, 8):
+            r = row + incy * i
+            c = col + incx * i
+
+            if (r < 0) or (r > 7) or (c < 0) or (c > 7):
+                break
+
+            sequence.append(self.state[r][c])
+
+        count = 0
+        for i in range(len(sequence)):
+            if me == 1:
+                if sequence[i] == 2:
+                    count += 1
+                else:
+                    if (sequence[i] == 1) and (count > 0):
+                        return True
+                    break
+            else:
+                if sequence[i] == 1:
+                    count += 1
+                else:
+                    if (sequence[i] == 2) and (count > 0):
+                        return True
+                    break
+
+        return False
+
+    def couldBe(self, row, col, me):
+        for incx in range(-1, 2):
+            for incy in range(-1, 2):
+                if (incx == 0) and (incy == 0):
+                    continue
+
+                if self.checkDirection(row, col, incx, incy, me):
+                    return True
+
+        return False
+
+    # generates the set of valid moves for the player; returns a list of valid moves (validMoves)
+    def getValidMoves(self, roundNum, me):
+        validMoves = []
+        print "Round: " + str(roundNum)
+
+        for i in range(8):
+            print self.state[i]
+
+        if roundNum < 4:
+            if self.state[3][3] == 0:
+                validMoves.append([3, 3])
+            if self.state[3][4] == 0:
+                validMoves.append([3, 4])
+            if self.state[4][3] == 0:
+                validMoves.append([4, 3])
+            if self.state[4][4] == 0:
+                validMoves.append([4, 4])
+        else:
+            for i in range(8):
+                for j in range(8):
+                    if self.state[i][j] == 0:
+                        if self.couldBe(i, j, me):
+                            validMoves.append([i, j])
+
+        return validMoves
